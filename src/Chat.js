84474e000d6932ca9566be9d1175bffda0a5c9d6
Shin-sibainu <shincode0712@gmail.com>
2022-06-18 09:38:04 +0900

    must css mod

---
+++ src/Chat.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { useScrollToBottom } from "react-scroll-to-bottom";
+
+const Chat = ({ socket, username, room }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        message: currentMessage,
+        author: username,
+        room: room,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList([...messageList, messageData]);
+      setCurrentMessage("");
+    }
+  };
+
+  //ソケット通信が行われる毎に発火する。次はここから
+
+  socket.on("receive_message", (data) => {
+    console.log(data);
+    setMessageList([...messageList, data]);
+  });
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>ライブチャット</p>
+      </div>
+      <div className="chat-body">
+        {messageList.map((messageContent) => (
+          <div
+            className="message"
+            id={username == messageContent.author ? "you" : "other"}
+          >
+            <div>
+              <div className="message-content">
+                <p>{messageContent.message}</p>
+              </div>
+              <div className="message-meta">
+                <p id="time">{messageContent.time}</p>
+                <p id="author">{messageContent.author}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          placeholder="Hey..."
+          onChange={(e) => setCurrentMessage(e.target.value)}
+          onKeyDown={(e) => {
+            e.key === "Enter" && sendMessage();
+          }}
+          value={currentMessage}
+        />
+        <button onClick={() => sendMessage()}>&#9658;</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
